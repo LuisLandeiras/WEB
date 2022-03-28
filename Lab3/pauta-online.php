c410eb4bb861b343be441c851de88d5ea5837ba4
Landeiras <landeirasluis@gmail.com>
2022-03-28 19:22:11 +0100

    Finished

--- Lab3/pauta-online.php
+++ Lab3/pauta-online.php
@@ -1,23 +1,21 @@
 <?php
 
-//$css = file_get_contents('pauta.css');
-
-$xml= simplexml_load_file('pauta.xml');
+$xml= simplexml_load_file('C:\Users\Landeiras\Documents\GitHub\WEB\Lab3\pauta.xml');
 
 $nota = 0;
 
-for($i = 0; $i<=3; $i++){
-    echo "Número: " .$xml->aluno[$i]."\n";
-    echo "Nome: " .$xml->aluno[$i]->nome."\n";
-    echo "Exame 1: " .$xml->aluno[$i]->exam1."\n";
-    echo "Exame 2: " .$xml->aluno[$i]->exam2."\n";
+for($i = 0; $i<=4; $i++){
+    echo 'Número: ' .$xml->aluno[$i]->attributes()."\n";
+    echo 'Nome: ' .$xml->aluno[$i]->nome."\n";
+    echo 'Exame 1: ' .$xml->aluno[$i]->exam1."\n";
+    echo 'Exame 2: ' .$xml->aluno[$i]->exam2."\n";
     $nota = ($xml->aluno[$i]->exam1 + $xml->aluno[$i]->exam2)/2;
-    echo "Nota Final: " .$nota."\n";
+    echo 'Nota Final: ' .$nota."\n";
     echo "\n";
     if($nota < 9.5){
         echo "Aluno/a reprovado/a\n";
     }else{
-        echo "Aluno/a reprovado/a\n";
+        echo "Aluno/a aprovado/a\n";
     }
     echo "\n";
 }
