cc902eecffb5472c1ab5c86ba1188af191e71b52
LuisLandeiras <landeirasluis@gmail.com>
2022-04-10 18:05:50 +0100

    Finished

--- Lab4/Lab4.1/Personagens.js
+++ Lab4/Lab4.1/Personagens.js
@@ -1,3 +1,7 @@
-function nome(){
-    if()
+function quote(str){
+    if(str == "Bart"){
+        return "Ay Caramba!";
+    }else if(str == "Homer") {
+        return "D'Oh!";
+    }
 }
---
+++ Lab4/Lab4.2/Figuras.js
@@ -0,0 +1,16 @@
+function figuras(x){
+    switch(x){
+        case 5:
+            return "pentágono";
+        case 6:
+            return "hexagono";
+        case 7:
+            return "heptagono";
+        case 8:
+            return "octagono";
+        case 9:
+            return "eneagono";
+        case 10:
+            return "decagono";
+    }
+}
