593eba371a5b959061d6df312bf5b903df372691
LuisLandeiras <landeirasluis@gmail.com>
2022-04-16 20:46:24 +0100

    Finished

--- AssLab3/AssLab3.3/CalcArea.js
+++ AssLab3/AssLab3.3/CalcArea.js
@@ -1,6 +1,18 @@
-function calcarea(area){
-    for(let i = 2; i<=10; i++){
-        area = PI * (i*i) + "<br>";
-        console.log(area)
+var PI = 3.1416;
+function area(a){
+    return Math.floor(PI * a * a);
+}
+function areaciclo(){
+    for(var i = 2; i <= 10; i++){
+        console.log(area(i));
+    }
+}
+function calcarea(){
+    let r = document.getElementById("radius").value;
+    document.getElementById("resultado").innerText = "Área igual:" + area(r);
+}
+function text(){
+    for (var i = 2; i <= 10; i++) {
+        document.getElementById("text").value = document.getElementById("text").value + area(i) + "\n";
     }
 }
