921f13e8df15c0994b298cdb11b38b4adbc85798
Landeiras <landeirasluis@gmail.com>
2022-04-27 00:41:49 +0100

    In Process

--- Project/Script/index.js
+++ Project/Script/index.js
@@ -1,6 +1,6 @@
 function equal(){
-    var a = document.getElementById("pass").innerHTML;
-    var b = document.getElementById("repitepass").innerHTML;
+    var a = document.getElementById("pass").value;
+    var b = document.getElementById("repitepass").value;
     if(b != a){
         alert("É necessário que as senhas sejam iguais");
     }
