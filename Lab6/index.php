3f989427b814fa5e785c527fafa5e3da3beb8092
Landeiras <landeirasluis@gmail.com>
2022-05-01 15:35:50 +0100

    In Process

---
+++ Lab6/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP Program</title>
+    </head>
+    <body>
+        <?php
+            print("Hello, World <br/>\n");
+            phpinfo();
+        ?>
+    </body>
+</html>
